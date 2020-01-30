9f88c358fd00cf02ddf7dd5f02734ea3aae54d38
PavelSlastenov <p.slastenov@mail.ru>
2020-01-30 19:10:26 +0300

    Создал файл для улучшений и дорисовал всплывающее облако с результатами

---
+++ js/stat.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;                        //По оси влево
+var CLOUD_Y = 10;                        //По оси вверх
+var GAP = 10;                           //Отступ
+var FONT_GAP = 25;                     //Отступ у текста
+var TEXT_HEIGHT = 40;                 //Высота текста
+var COLUMN_HEIGHT = 150;             //Высота гистограммы
+var barWidth = 40;                  //Ширина колонки
+var COLUMN_SPACE = 50;             //Расстояние между колонками
+
+//Функция отрисовки облака
+var renderCloud = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;                                         //Цвет перенесли в параметры
+  ctx.fillRect(x, y, width, height);
+};
+
+//Параметры текста
+var drawsText = function (ctx, text, x, y, color, font) {
+  ctx.fillStyle = color;
+  ctx.font = '16px PT Mono';
+  ctx.fillText(text, x, y);
+};
+
+//Находит максимальное время в масиве
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];                                //Отмечает первый элемент как максимальный
+
+  for (var i = 0; i < arr.length; i++) {                //Переберает элементы в массиве и сравнивает с максимальным
+    if (arr[i] > maxElement) {                         //Находит элемент больше максимального
+      maxElement = arr[i];                            //Записывает найденный элемент как максимальный
+    }
+  }
+
+  return maxElement;                               //Возвращает максимальный элемент
+};
+
+//Отрисовка тени, игроков, затраченного времени
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');   //Тень от облака и цвет
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');                            //Облако и цвет
+
+  ctx.fillStyle = '#000';                                                                         //Цвет текста сверху
+
+  var maxTime = getMaxElement(times);                                                  //Алгоритм поиска максимального элемента в массиве times
+  var scaleFactor = COLUMN_HEIGHT / maxTime;                                          //Определяет масштаб: высоту гистограммы делим на максимальное время
+
+  drawsText(ctx, 'Ура вы победили!', 230, 25);                                      //Указал координаты числами, что бы надпись "Ура" не скрывалась за уже нарисованным облаком в игре
+  drawsText(ctx, 'Список результатов:', CLOUD_X + CLOUD_Y, GAP + TEXT_HEIGHT);
+
+  for (var i = 0; i < players.length; i++) {                                      //Цикл который изменяет значение переменной i(index) в которой будет храниться порядковый номер текущего элемента массива от 0 до длины массива
+    var barHeight = scaleFactor * times[i];
+    var x = CLOUD_X + GAP + COLUMN_SPACE + (barWidth + COLUMN_SPACE) * i;       //Формула отрисовки столбца
+    var y = CLOUD_X + GAP + COLUMN_HEIGHT - barHeight;
+
+
+    renderCloud(ctx, x, y - FONT_GAP, barWidth, barHeight, players[i] === 'Вы' ? 'rgba(255, 0 ,0, 1)' : 'hsl(240,' + Math.random() * 100 + '%, 50%)');   //Задает отдельно цвет игрок "Вы"
+                                                                                                                                                        //остальные игроки синие, с случайно задающейся насыщенностью через "hsl"
+    drawsText(ctx, players[i], x, CLOUD_HEIGHT - GAP);
+    drawsText(ctx, Math.round(times[i]), x, y - TEXT_HEIGHT);
+  }
+
+};
