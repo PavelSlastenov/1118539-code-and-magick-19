952271b17481bcbb2327000a00129f09134cda7f
PavelSlastenov <p.slastenov@mail.ru>
2020-02-14 10:05:15 +0300

    Создаёт новый файл

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,7 @@ var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var lastName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var randomElement = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+status
 
 var similarListElement = document.querySelector('.setup-similar-list');
 document.querySelector('.setup').classList.remove('hidden');
